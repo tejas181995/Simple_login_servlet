72205aed30d95fc0ce070cc9a4ea7e7ad6121b6c
Tejas <tejasdev95@gmail.com>
2021-04-21 17:04:13 +0530

    simple login servlet page created

---
+++ src/main/java/com/firstservlet/LoginServlet.java
@@ -0,0 +1,41 @@
+package com.firstservlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(
+        description = "Login Servlet Testing",
+        urlPatterns = {"/LoginServlet"},
+        initParams = {
+                @WebInitParam(name = "user", value = "Tejas"),
+                @WebInitParam(name = "password", value = "123456")
+        }
+)
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //get request parameters for userID and password
+        String user = request.getParameter("user");
+        String pwd = request.getParameter("pwd");
+        //get servlet config init params
+        String userID = getServletConfig().getInitParameter("user");
+        String password = getServletConfig().getInitParameter("password");
+        if (userID.equals(user) && password.equals(pwd) ) {
+            request.setAttribute("user", user);
+            request.getRequestDispatcher("loginSuccess.jsp").forward(request, response);
+        } else {
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out = response.getWriter();
+            out.println("<font color=red>Either user name or password is wrong.</font> ");
+            rd.include(request, response);
+        }
+    }
+
+}
